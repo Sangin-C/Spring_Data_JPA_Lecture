ed7321f563e0de7dd983b15aee3763296cba5ccf
Sangin-C <sangin3435@naver.com>
2021-09-27 23:24:50 +0900

    챕터3. 공통 인터페이스 기능 - 리포지토리 만들기

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,45 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String username;
 
-    protected Member() {
-    }
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+
+    //연관과계 편의 메서드
+    public void changeTeam(Team team) {
+        this.team = team;
+        //Team에있는 Member도 바뀌어야 하기 때문에 셋팅해줘야함.
+        team.getMembers().add(this);
     }
 }
