2c27c42bc2d48669f5898dbf484bf13286c51fa6
Sangin-C <sangin3435@naver.com>
2021-10-08 16:25:48 +0900

    챕터5. 확장 기능 - Web 확장 ( 페이징, 정렬 )

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,51 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+import java.nio.channels.MembershipKey;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    //HTTP요청은 회원id를 받지만 도메인 클래스 컨버터가 중간에 동작해 회원 엔티티 객체를 반환해준다.
+    //도메인 클래스 컨버터로 엔티티를 파라미터로 받으면, 이 엔티티는 조회용으로만 사용해야한다.
+    @GetMapping("/members2/{id}")
+    public String findMember(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        Page<MemberDto> map = page.map(member -> new MemberDto(member));
+        return map;
+    }
+
+
+    @PostConstruct
+    public void init() {
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
+    }
+}
