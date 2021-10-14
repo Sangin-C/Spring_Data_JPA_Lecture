2d2ace68b480801e92e3aa0340e41a8096f4b1ad
Sangin-C <sangin3435@naver.com>
2021-10-14 22:19:28 +0900

    챕터6. 기타 기능들 - Query By Example ( Matcher )

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -5,9 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -311,6 +309,8 @@ public class MemberRepositoryTest {
 
     @Test
     public void specBasic() {
+
+        //given
         Team teamA = new Team("teamA");
         em.persist(teamA);
 
@@ -322,11 +322,44 @@ public class MemberRepositoryTest {
         em.flush();
         em.clear();
 
-
+        //when
         Specification<Member> spec = MemberSpec.username("m1").and(MemberSpec.teamName("teamA"));
         List<Member> result = memberRepository.findAll(spec);
 
-        Assertions.assertThat(result.size()).isEqualTo(1);
+        assertThat(result.size()).isEqualTo(1);
+
+
+    }
+
+    @Test
+    public void queryByExample() {
+
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        //Probe
+        Member member = new Member("m1");
+        Team team = new Team("teamA");
+        member.setTeam(team);
+
+        //조건에서 제외할 수 있다. null인건 기본으로 제외되지만 primitive타입은 0으로 값이 들어가기때문에 제외를 시켜줘야한다.
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnorePaths("age");
+
+        Example<Member> example = Example.of(member, matcher);
+
+        List<Member> result = memberRepository.findAll(example);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("m1");
 
 
     }
